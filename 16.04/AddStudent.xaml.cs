9bb14c4eab74dc2103b1c10cbd7879ee9a52d563
norbertc5 <113235406+norbertc5@users.noreply.github.com>
2025-04-16 13:02:28 +0200

    16.04
    Base validation

---
+++ 16.04/AddStudent.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class AddStudent : Window
+    {
+        MainWindow mainWindow;
+
+        public AddStudent(MainWindow _mainWindow)
+        {
+            InitializeComponent();
+            mainWindow = _mainWindow;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            /*if(!idBox.Text.All(c => Char.IsNumber(c)))
+            {
+                idBox.Background = Brushes.Red;
+                return;
+            }
+            else
+            {
+                idBox.Background = Brushes.White;
+            }*/
+
+            string nameContent = nameBox.Text.Trim();
+            string surnameContent = surnameBox.Text.Trim();
+            string peselContent = peselBox.Text.Trim();
+            string secondNameContent = secondNameBox.Text.Trim();
+            string dobContent = dobBox.Text.Trim();
+            string phoneContent = phoneBox.Text.Trim();
+            string adressContent = adressBox.Text.Trim();
+            string placeContent = placeBox.Text.Trim();
+            string postCodeContent= postCodeBox.Text.Trim();
+
+            bool isInvalid = false;
+
+            if(nameContent == "")
+            {
+                nameBox.Background = Brushes.Red;
+                isInvalid = true;
+            }
+            else
+            {
+                nameBox.Background = Brushes.White;
+                nameContent = CapitalOnBeginnign(nameContent); 
+            }
+
+            if (surnameContent == "")
+            {
+                surnameBox.Background = Brushes.Red;
+                isInvalid = true;
+            }
+            else
+            {
+                surnameBox.Background = Brushes.White;
+                surnameContent = CapitalOnBeginnign(surnameContent);
+            }
+
+            if(peselContent == "")
+            {
+                peselBox.Background = Brushes.Red;
+                isInvalid = true;
+            }
+            else
+            {
+                peselBox.Background = Brushes.White;
+            }
+
+            if(dobContent == "")
+            {
+                dobBox.Background = Brushes.Red;
+                isInvalid = true;
+            }
+            else
+            {
+                dobBox.Background = Brushes.White;
+            }
+
+            if(adressContent == "")
+            {
+                adressBox.Background = Brushes.Red;
+                isInvalid = true;
+            }
+            else
+            {
+                adressBox.Background = Brushes.White;
+            }
+
+            if (placeContent == "")
+            {
+                placeBox.Background = Brushes.Red;
+                isInvalid = true;
+            }
+            else
+            {
+                placeBox.Background = Brushes.White;
+                placeContent = CapitalOnBeginnign(placeContent);
+            }
+
+            if(postCodeContent == "")
+            {
+                postCodeBox.Background = Brushes.Red;
+                isInvalid = true;
+            }
+            else
+            {
+                postCodeBox.Background = Brushes.White;
+            }
+
+            if (isInvalid)
+                return;
+
+
+            mainWindow.AddRecord(1, nameContent, surnameContent, peselContent, secondNameContent,
+                dobContent, phoneContent, adressContent, placeContent, postCodeContent);
+        }
+
+        /// <summary>
+        /// Makes string starts with capital letter
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        string CapitalOnBeginnign(string s)
+        {
+            return s = s[0].ToString().ToUpper() + s.Remove(0, 1);
+        }
+    }
+}
---
+++ 16.04/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public List<Record> records = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddStudent addStudentWindow = new AddStudent(this);
+            addStudentWindow.Show();
+        }
+
+        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        public void AddRecord(int _id, string _name, string _surname, string _pesel, 
+            string _secondName, string _dob, string _phone, string _adress, string _place, string _postCode)
+        {
+            records.Add(new Record() { id = _id, name = _name, surname = _surname, pesel = _pesel, secondName = _secondName, dob = _dob, phone = _phone, adress = _adress, 
+            place = _place, postCode = _postCode});
+            listView.Items.Add(records[records.Count - 1]);
+        }
+    }
+
+    public class Record
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public string? surname { get; set; }
+        public string? pesel { get; set; }
+        public string? secondName { get; set; }
+        public string? dob{ get; set; }
+        public string? phone { get; set; }
+        public string? adress { get; set; }
+        public string? place { get; set; }
+        public string? postCode { get; set; }
+    }
+}
